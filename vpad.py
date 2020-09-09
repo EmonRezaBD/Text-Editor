44a414f2e8ac6c99b525c1104e977aa08523c11c
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2020-09-10 00:43:04 +0600

    initial

---
+++ vpad.py
@@ -0,0 +1,49 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import font, colorchooser, filedialog, messagebox
+import os
+
+main_application = tk.Tk()
+main_application.geometry('1200x800')
+main_application.title('Vpad Text editor')
+
+########################### main menu ########################
+#-------------------------  End main menu --------------------
+
+main_menu = tk.Menu()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+########################### main menu ########################
+#-------------------------  End main menu --------------------
+
+
+
+########################### toolbar ########################
+#-------------------------  End toolbar --------------------
+
+
+########################### text editor ########################
+#-------------------------  End text editor--------------------
+
+########################### main status bar ########################
+#-------------------------  End main status bar --------------------
+
+########################### main menu functionality########################
+#-------------------------  End main menu functionality--------------------
+
+main_application.config(menu=main_menu)
+main_application.mainloop()
