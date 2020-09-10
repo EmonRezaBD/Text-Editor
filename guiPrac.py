3ee7f990363a72580f95d3e5c35284f46d9be91e
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2020-09-10 20:31:31 +0600

    text box

--- guiPrac.py
+++ guiPrac.py
@@ -9,16 +9,27 @@ root.geometry('450x300')
 #adding a label to root window
 
 lbl = Label(root, text="Are u a geek?")
-lbl.grid()
+lbl.grid(column =1 , row=0 )
+
+#adding entry field
+txt = Entry(root, width=10)
+txt.grid(column = 1, row = 1)
 
 #function to display text when
 #button is clicked
 
+lebel = Label(root, text = " ")
+lebel.grid(column=0,row=3)
+
 def clicked():
-	lbl.configure(text = "I just got thap")
+	res = "You wrote "+txt.get()
+	lebel.configure(text = res)
 
 btn = Button(root, text = "Click me", fg = "red" , command=clicked)
 
-btn.grid(column=1, row=0)
+btn.grid(column=0, row=1)
+
+
+
 
 root.mainloop()
