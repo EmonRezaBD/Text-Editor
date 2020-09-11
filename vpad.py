d34edfa1c34e7588826b0f9191aee5d88aea73af
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2020-09-11 17:04:17 +0600

    edit complete

--- vpad.py
+++ vpad.py
@@ -31,9 +31,29 @@ file.add_command(label='Save',image=save_icon,compound=tk.LEFT, accelerator='Ctr
 file.add_command(label='Save as',image=save_as_icon,compound=tk.LEFT, accelerator='Ctrl+Alt+S')
 file.add_command(label='Exit',image=exit_icon,compound=tk.LEFT, accelerator='Ctrl+Q')
 
+####Edit
+##edit icons
+copy_icon = tk.PhotoImage(file='icons2/copy.png')
+paste_icon = tk.PhotoImage(file='icons2/paste.png')
+copy_icon = tk.PhotoImage(file='icons2/copy.png')
+cut_icon = tk.PhotoImage(file='icons2/cut.png')
+clear_all_icon = tk.PhotoImage(file='icons2/clear_all.png')
+find_icon = tk.PhotoImage(file='icons2/find.png')
 
 
 edit = tk.Menu(main_menu, tearoff=False)
+
+
+edit.add_command(label='Copy',image=copy_icon, compound=tk.LEFT, accelerator='Ctrl+C')
+edit.add_command(label='Paste',image=paste_icon, compound=tk.LEFT, accelerator='Ctrl+P')
+edit.add_command(label='Cut',image=cut_icon, compound=tk.LEFT, accelerator='Ctrl+X')
+edit.add_command(label='Clear All',image=clear_all_icon, compound=tk.LEFT, accelerator='Ctrl+Alt+X')
+edit.add_command(label='Find',image=find_icon, compound=tk.LEFT, accelerator='Ctrl+F')
+
+##view icons
+tool_bar_icon = tk.PhotoImage(file='icons2/tool_bar.png')
+
+
 view = tk.Menu(main_menu, tearoff=False)
 color_theme = tk.Menu(main_menu, tearoff=False)
 
