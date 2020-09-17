72c6f6fda64bdf7e85a33cf6ce3397cb254533d8
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2020-09-16 22:37:23 -0700

    copy paste done

--- vpad.py
+++ vpad.py
@@ -367,10 +367,14 @@ file.add_command(label='Exit',image=exit_icon,compound=tk.LEFT, accelerator='Ctr
 
 ##edit command
 
-edit.add_command(label='Copy',image=copy_icon, compound=tk.LEFT, accelerator='Ctrl+C')
-edit.add_command(label='Paste',image=paste_icon, compound=tk.LEFT, accelerator='Ctrl+P')
-edit.add_command(label='Cut',image=cut_icon, compound=tk.LEFT, accelerator='Ctrl+X')
-edit.add_command(label='Clear All',image=clear_all_icon, compound=tk.LEFT, accelerator='Ctrl+Alt+X')
+
+
+
+
+edit.add_command(label='Copy',image=copy_icon, compound=tk.LEFT, accelerator='Ctrl+C', command=lambda:text_editor.event_generate("<Control c>"))
+edit.add_command(label='Paste',image=paste_icon, compound=tk.LEFT, accelerator='Ctrl+V', command=lambda:text_editor.event_generate("<Control v>"))
+edit.add_command(label='Cut',image=cut_icon, compound=tk.LEFT, accelerator='Ctrl+X', command=lambda:text_editor.event_generate("<Control x>"))
+edit.add_command(label='Clear All',image=clear_all_icon, compound=tk.LEFT, accelerator='Ctrl+Alt+X', command = lambda:text_editor.delete(1.0,tk.END)) ###Keyboard button isn't working 
 edit.add_command(label='Find',image=find_icon, compound=tk.LEFT, accelerator='Ctrl+F')
 
 
